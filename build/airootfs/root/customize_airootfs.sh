caa0cde96961fa1c4255d0231a6dbbf58ddf02b8
surf60 <131695480+surf60@users.noreply.github.com>
2023-04-25 22:24:53 +0100

    gets user with no home dir

--- build/airootfs/root/customize_airootfs.sh
+++ build/airootfs/root/customize_airootfs.sh
@@ -1,6 +1,3 @@
-#useradd -m -G wheel -s /bin/bash surfos
-#echo \"surfos:surfos\"|chpasswd
-#echo \"root:root\"|chpasswd
 systemctl enable NetworkManager
 systemctl enable sshd
 systemctl enable sddm
