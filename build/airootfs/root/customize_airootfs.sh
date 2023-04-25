ed2b02202e31885ef5c9a360e82c159f6df259e3
surf60 <131695480+surf60@users.noreply.github.com>
2023-04-25 15:57:29 +0100

    made users

--- build/airootfs/root/customize_airootfs.sh
+++ build/airootfs/root/customize_airootfs.sh
@@ -1,6 +1,6 @@
-useradd -m -G wheel -s /bin/bash surfos
-echo \"surfos:surfos\"|chpasswd
-echo \"root:root\"|chpasswd
+#useradd -m -G wheel -s /bin/bash surfos
+#echo \"surfos:surfos\"|chpasswd
+#echo \"root:root\"|chpasswd
 systemctl enable NetworkManager
 systemctl enable sshd
 systemctl enable sddm
